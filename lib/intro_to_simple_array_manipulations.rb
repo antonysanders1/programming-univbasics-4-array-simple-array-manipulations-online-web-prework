5d033692ad11652be9fe21fbbb8f0ae76dc7f274
Antony Sanders <antony.devon92@gmail.com>
2019-10-26 22:37:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,14 +13,47 @@ def using_pop(continents)
     deleted_string = @continents.pop
 end
 
+
 def pop_with_args(dog_breeds)
   small_dogs = @dog_breeds.pop(2)
 end
+
 
 def using_shift(my_favorite_cities)
   im_so_over_this_city = @my_favorite_cities.shift
 end
 
+
 def shift_with_args(ice_cream_brands)
   brands_removed = @ice_cream_brands.shift(2)
+end
+
+
+def using_concat
+  
+end
+
+
+def using_insert
+  
+end
+
+
+def using_uniq
+  
+end
+
+
+def using_flatten
+  
+end
+
+
+def using_delete
+  
+end
+
+
+def using_delete_at
+  
 end
