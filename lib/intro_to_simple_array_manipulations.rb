013feab2316100b44e3cf53be0dc7f9455dadcdd
Antony Sanders <antony.devon92@gmail.com>
2019-10-26 22:18:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,7 +9,7 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
 end
 
 
-def using_pop(@continents)
+def using_pop(continents)
   @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
     deleted_string = @continents.pop
 end
