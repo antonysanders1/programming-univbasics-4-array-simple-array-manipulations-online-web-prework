17772b67ccec80225fa36c4a4b86cc65719ad7d7
Antony Sanders <antony.devon92@gmail.com>
2019-10-26 22:54:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,8 +35,8 @@ def using_concat(my_favorite_things, more_favs)
 end
 
 
-def using_insert(list_of_programming_languages, anther_languge)
-  .insert( ,)
+def using_insert(list_of_programming_languages, another_languge)
+  @list_of_programming_languages.insert(4, another_language)
 end
 
 
