ea73da5812c778edefd94e24c5413f3c1909e3f4
Antony Sanders <antony.devon92@gmail.com>
2019-10-26 22:26:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,6 +15,5 @@ def using_pop(continents)
 end
 
 def pop_with_args(dog_breeds, small_dogs)
-  small_dogs = ["Chihuahua", "Shiba Inu"]
-  dog_breeds.pop(@small_dogs)
+  small_dogs = dog_breeds.pop(2)
 end
