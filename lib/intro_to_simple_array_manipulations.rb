96228ea7a42e9b30ecc49b9c8f6841fd9cbbbde9
Antony Sanders <antony.devon92@gmail.com>
2019-10-26 21:22:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,25 @@ def using_push (colors_in_the_rainbow, next_color)
   updated_array = colors_in_the_rainbow.push << next_color
 end
 
+
 def using_unshift
+  
+end
+
+
+def using_pop
+  
+end
+
+
+def pop_with_args
+  
+end
+
+
+def using_shift
+  
+end
+
+
+def using_shift_with_args
