86c34465a3d7c6bd41433baa8d0f8487e38e8685
Antony Sanders <antony.devon92@gmail.com>
2019-10-26 23:07:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -50,8 +50,8 @@ def using_flatten(instruments)
 end
 
 
-def using_delete(instructors, "Steven")
-  
+def using_delete(instructors, no_offense_steven)
+  no_offense_steven = "Steven"
   instructors.delete("Steven")
 end
 
